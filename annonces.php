68910dd1b2000af69e1a825f982fdee9936ee717
Fabien Lamotte <Vigaaa@232.202.portail.wifi.u-picardie.fr>
2017-01-03 09:15:58 +0100

    modif annulée

--- annonces.php
+++ annonces.php
@@ -66,7 +66,7 @@ if (!$session->userIsLogged()) {
                <div class="form-group">
                   <label for="ville">Nb de joueurs</label>
                   <select>
-                     
+                     <option value="1">1</option>
                      <option value="2">2</option>
                      <option value="3">3</option>
                      <option value="4">4</option>
